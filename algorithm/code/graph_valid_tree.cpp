03053553b2887704e009e7f40ed557e22cf50680
T-tssxuan <2270360114@qq.com>
2016-11-01 00:35:12 +0800

    [new] method for check the graph is tree

---
+++ algorithm/code/graph_valid_tree.cpp
@@ -0,0 +1,77 @@
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <assert.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param n an integer
+     * @param edges a list of undirected edges
+     * @return true if it's a valid tree, or false
+     */
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (n == 0 || n == 1) {
+            return true;
+        }
+        if (edges.size() == 0 && n > 0) {
+            return false;
+        }
+        vector<int> record(n, 0);
+        for (int i = 0; i < n; ++i) {
+            record[i] = i;
+        }
+        int min, max;
+        for (auto& edge : edges) {
+            if (record[edge[0]] != record[edge[1]]) {
+                min = record[edge[0]] < record[edge[1]]? \
+                      record[edge[0]] : record[edge[1]];
+                max = record[edge[0]] > record[edge[1]]? \
+                      record[edge[0]] : record[edge[1]];
+                for (int i = 0; i < n; ++i) {
+                    if (record[i] == max) {
+                        record[i] = min;
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+        for (auto ele : record) {
+            if (ele != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution so;
+    vector<int> test;
+    int n, tmp;
+    while (cin >> n) {
+        test.clear();
+        while (n-- > 0) {
+            cin >> tmp;
+            test.push_back(tmp);
+        }
+        int re = so.maxCoins(test);
+        cout << re << endl;
+    }
+    return 0;
+}
